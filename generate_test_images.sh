e129fcf1da83d9ac4fa10f835a4ed9e342fc7f99
Akshit Achara <aac24@bioeng393-pc.isd.kcl.ac.uk>
2025-10-21 19:37:19 +0100

    300 images per concept/prompt to be generated

--- generate_test_images.sh
+++ generate_test_images.sh
@@ -1,17 +1,15 @@
 # the concepts here will be the ones to generate
 concepts=("CEO" "doctor" "pilot" "technician" "fashion designer" "librarian" "teacher" "nurse")
-models=("sdxl")
+models=("sd21" "sdxl")
 
 for model in "${models[@]}"; do
     for concept in "${concepts[@]}"; do
         # concept
         CUDA_VISIBLE_DEVICES=1 python -m diffusion.run_with_steering --model_name $model \
-        --generate_concept $concept  --num_images_per_prompt 20  --steering_method casteer \
-        --steering_strength 2  --intermediate_clipping --output_dir ../outputs_${model}_${concept}/ translate  \
-        --source_concept_path ../outputs_sdxl/pos_means_30.pickle \
-        --target_concept_path ../outputs_sdxl/neg_means_30.pickle \
+        --generate_concept $concept  --num_images_per_prompt 300  --steering_method casteer \
+        --steering_strength 1 --output_dir ../outputs_${model}_${concept}/ translate  \
+        --source_concept_path ../outputs_${model}/pos_means_30.pickle \
+        --target_concept_path ../outputs_${model}/neg_means_30.pickle \
         --attribute gender
-
     done
-
 done
